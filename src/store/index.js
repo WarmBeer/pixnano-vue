2faf0387635521610cc1cea88a2c3ffd02a3f1e2
Mick <mickvd99@gmail.com>
2021-02-04 14:48:08 +0100

    Added vuex

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+export default new Vuex.Store({
+    state: {
+        // put variables and collections here
+    },
+    getters: {
+        // put sychronous functions for changing state e.g. add, edit, delete
+    },
+    mutations: {
+        // put sychronous functions for changing state e.g. add, edit, delete
+    },
+
+})
